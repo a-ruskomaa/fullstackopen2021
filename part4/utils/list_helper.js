af7e5fd5ebc6807e6e6fa5a126aae54a044669eb
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-07-28 20:47:48 +0300

    Add favorite blog helper function + tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.sort((a, b) => a.likes - b.likes).pop()
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
