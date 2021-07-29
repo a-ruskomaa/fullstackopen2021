ade80600dbf44025883d0337a9f82d298e007390
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-07-29 22:20:22 +0300

    Test that correct number of blogs is returned

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const { initialBlogs } = require('./helper')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-describe('blog api', () => {
-  test('returns all blogs as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+describe('get all blogs', () => {
+  test('content type is json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
   })
 })
 
