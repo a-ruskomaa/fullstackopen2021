9a6895b92ae85a9105f64d379735dc1356029e3c
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-07-30 20:03:04 +0300

    Add test cases for missing token

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -196,6 +196,13 @@ describe('DELETE /api/blogs/:id', () => {
       .expect(204)
   })
 
+  test('returns not authorized for missing token', async () => {
+    const blogs = await blogsInDb()
+    const id = blogs[0].id
+
+    await api.delete(`/api/blogs/${id}`).expect(401)
+  })
+
   test('deletes the correct post', async () => {
     const blogs = await blogsInDb()
     const firstBlog = blogs[0]
@@ -252,6 +259,17 @@ describe('PUT /api/blogs/:id', () => {
     )
 
     expect(updatedBlogInDb).toHaveProperty('likes', 42)
+  })
+
+  test('returns not authorized for missing token', async () => {
+    const blogs = await blogsInDb()
+    const firstBlog = blogs[0]
+    const updatedBlog = {
+      ...firstBlog,
+      likes: 42,
+    }
+
+    await api.put(`/api/blogs/${firstBlog.id}`).send(updatedBlog).expect(401)
   })
 })
 
