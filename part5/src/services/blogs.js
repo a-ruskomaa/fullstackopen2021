935a23e413ae526d00dd548b8a6ef5cc39aa9551
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-07-31 21:14:09 +0300

    Refactor blogService to a named object

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const blogService = {
+  getAll: () => {
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
+  },
 }
 
-export default { getAll }
+export default blogService
