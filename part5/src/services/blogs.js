1e3b93771caa75312915a49543c0bd9b40692c39
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-07-31 23:01:02 +0300

    Add create method to blogservice

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -2,9 +2,22 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 
 const blogService = {
-  getAll: () => {
+  token: null,
+  setToken: function (newToken) {
+    this.token = `bearer ${newToken}`
+  },
+  getAll: async function () {
     const request = axios.get(baseUrl)
-    return request.then((response) => response.data)
+    const response = await request
+    return response.data
+  },
+  create: async function (newObject) {
+    const config = {
+      headers: { Authorization: this.token },
+    }
+
+    const response = await axios.post(baseUrl, newObject, config)
+    return response.data
   },
 }
 
