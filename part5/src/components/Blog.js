38fb8cea59c53fc952fdc3451f726ab341ce7af1
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-08-01 12:32:28 +0300

    Add detailed view to blogs

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [detailedView, setDetailedView] = useState(false)
+
+  const toggleDetailedView = () => {
+    setDetailedView(!detailedView)
+  }
+
+  return (
+    <div className="blog-item">
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetailedView}>
+          {detailedView ? 'hide' : 'show'}
+        </button>
+      </p>
+      {detailedView ? (
+        <>
+          <p>
+            likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </>
+      ) : null}
+    </div>
+  )
+}
 
 export default Blog
