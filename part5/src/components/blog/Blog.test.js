b21e63bb146b32e65fa7f7ed235b6c67afd7067c
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-08-13 20:14:14 +0300

    Test that url and likes are rendered after toggle button click

--- part5/src/components/blog/Blog.test.js
+++ part5/src/components/blog/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -8,7 +8,7 @@ describe('<Blog />', () => {
 
   const blog = {
     id: 'id',
-    user: 'user',
+    user: { name: 'user' },
     likes: 13,
     author: 'author',
     title: 'title',
@@ -27,5 +27,14 @@ describe('<Blog />', () => {
     expect(component.container).not.toHaveTextContent('url')
 
     expect(component.container).not.toHaveTextContent('13')
+  })
+
+  test('renders url and likes after button click', () => {
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('url')
+
+    expect(component.container).toHaveTextContent('13')
   })
 })
