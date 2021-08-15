fb3c228eec0a2630872aa6996656d06bd9c1e240
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-08-15 20:49:06 +0300

    Add helper commands

---
+++ part5/cypress/support/commands.js
@@ -0,0 +1,26 @@
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedInUser', JSON.stringify(body))
+  })
+})
+
+Cypress.Commands.add('createUser', ({ username, name, password }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/users',
+    method: 'POST',
+    body: { username, name, password }
+  })
+})
+
+Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url, likes },
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedInUser')).token}`
+    }
+  })
+})
