ff9d088044b673bc886206f5105a62fea6054d54
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-08-15 20:49:15 +0300

    Test for login

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,11 @@
+const username = 'testUser'
+const name = 'Test User'
+const password = 'testpassword'
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.createUser({ username, name, password })
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +14,23 @@ describe('Blog app', function() {
     cy.get('#login-input-username').should('exist')
     cy.get('#login-input-password').should('exist')
     cy.get('#login-button-login').should('exist')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.visit('http://localhost:3000')
+      cy.get('#login-input-username').type(username)
+      cy.get('#login-input-password').type(password)
+      cy.get('#login-button-login').click()
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.visit('http://localhost:3000')
+      cy.get('#login-input-username').type('wrong')
+      cy.get('#login-input-password').type('wrong')
+      cy.get('#login-button-login').click()
+      cy.contains('wrong credentials')
+    })
   })
 })
