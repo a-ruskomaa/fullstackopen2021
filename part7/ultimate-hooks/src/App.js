1b21dd69dee8949c4c1628d0e5db3db3d00078c5
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2021-10-11 22:42:15 +0300

    useResource hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -20,14 +20,23 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    getAll(baseUrl)
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
